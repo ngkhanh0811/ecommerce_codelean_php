9f857040b8b83e660f990b0c131003f903a8d649
nguyenkhanh08112003 <91303308+nguyenkhanh08112003@users.noreply.github.com>
2022-08-23 09:05:55 +0700

    update

---
+++ ecmmerce/database/migrations/2022_08_18_023150_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('company_name');
+            $table->string('country');
+            $table->string('street_address');
+            $table->string('postcode_zip');
+            $table->string('town_city');
+            $table->string('email');
+            $table->string('phone');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
